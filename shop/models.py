c9dc78541c5d175e5a1b371e58bca782afdb864b
endang-ismaya <endang.ismaya@gmail.com>
2025-02-26 12:56:09 +0700

    first setup

---
+++ shop/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.FloatField()
+    students_qty = models.IntegerField()
+    reviews_qty = models.IntegerField()
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
