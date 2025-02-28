4aa8562a8d49b207bdbd3eb32117eddfd1a08afe
endang-ismaya <endang.ismaya@gmail.com>
2025-02-28 22:04:03 +0700

    add __str__ to models and regiter on admin.py

--- shop/models.py
+++ shop/models.py
@@ -6,6 +6,9 @@ class Category(models.Model):
     title = models.CharField(max_length=300)
     created_at = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return self.title
+
 
 class Course(models.Model):
     title = models.CharField(max_length=255)
@@ -13,3 +16,6 @@ class Course(models.Model):
     students_qty = models.IntegerField()
     reviews_qty = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
