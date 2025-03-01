db946f506e4272c1fff79c74daca13fbf6ebf6f3
endang-ismaya <endang.ismaya@gmail.com>
2025-03-01 16:37:31 +0700

    add single course page and using workspace name in urls

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from shop import views
 
+app_name = "shop"
+
 urlpatterns = [
-    path("", views.index, name="shop__index"),
+    path("", views.index, name="index"),
+    path("<int:course_id>", views.single_course, name="course"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from shop.models import Course
 
 
 def index(request):
     courses = Course.objects.all()
     return render(request, "shop/courses.html", context={"courses": courses})
+
+
+def single_course(request, course_id: int):
+    # course = Course.objects.get(pk=course_id)
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, "shop/single_course.html", {"course": course})
