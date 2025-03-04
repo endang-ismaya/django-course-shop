142048bdeb86b6d13363bfdd85bebae0b8337c9c
endang-ismaya <endang.ismaya@gmail.com>
2025-03-04 13:45:33 +0700

    add custom api.authentication

--- api/models.py
+++ api/models.py
@@ -1,5 +1,7 @@
 from tastypie.resources import ModelResource
 from shop.models import Category, Course
+from tastypie.authorization import Authorization
+from api.authentication import CustomApiKeyAuthentication
 
 
 class CategoryResource(ModelResource):
@@ -14,3 +16,18 @@ class CourseResource(ModelResource):
         queryset = Course.objects.all()
         resource_name = "courses"
         allowed_methods = ["get", "delete", "post"]
+        authentication = CustomApiKeyAuthentication()
+        authorization = Authorization()
+        always_return_data = True
+
+    def hydrate(self, bundle):
+        bundle.obj.category_id = bundle.data["category_id"]
+        return super().hydrate(bundle)
+
+    def dehydrate(self, bundle):
+        bundle.data["category_id"] = bundle.obj.category.id
+        bundle.data["category"] = bundle.obj.category
+        return super().dehydrate(bundle)
+
+    def dehydrate_title(self, bundle):
+        return bundle.data["title"].upper()
