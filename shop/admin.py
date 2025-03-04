1ca388b4dcd40e189dc0dba6deb8e8fa604563fb
endang-ismaya <endang.ismaya@gmail.com>
2025-03-04 19:12:05 +0700

    add admin model view at shop app

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from shop import models
+from shop.models import Category, Course
 
-admin.site.register(models.Category)
-admin.site.register(models.Course)
+admin.site.site_header = "Courses Admin"
+admin.site.site_title = "My Courses"
+admin.site.index_title = "Manage Courses"
+
+
+class CourseInline(admin.TabularInline):
+    model = Course
+    exclude = ["created_at"]
+    extra = 1
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "category")
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_at")
+    inlines = [CourseInline]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Course, CourseAdmin)
